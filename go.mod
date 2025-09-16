dc77a4b81fe857a78dac9dde0ac142649297b244
Aldo Camacho <me@aldocamacho.com>
2025-09-15 18:51:11 -0700

    hash password

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Quak1/gokei
 
 go 1.25.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.42.0
+)
---
+++ internal/auth/hashPassword.go
@@ -0,0 +1,18 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+const bcryptCost = 14
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcryptCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func CheckHashedPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
